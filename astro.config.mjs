ba558e3b5a09dafd9b0c8de01561f9f024e6030c
yang <juliansalvador.dev@gmail.com>
2026-06-10 01:35:10 +0800

    feat(layouts): extract app layout to its own component + implement playfair font asset

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,39 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.local(),
+        name: "Playfair Display",
+        cssVariable: "--font-playfair-display",
+        fallbacks: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+        options: {
+          variants: [
+            {
+              src: ["./src/assets/fonts/Playfair-Display-Variable.woff2"],
+              weight: "100 900",
+              style: "normal",
+              display: "swap",
+            },
+          ],
+        },
+      },
+    ],
+  },
 });
