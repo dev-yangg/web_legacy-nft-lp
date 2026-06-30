be507acd46281860db73b9d913c101a6bf5f9203
yang <juliansalvador.dev@gmail.com>
2026-06-30 19:02:13 +0800

    refactor(components): make Statistics component reusable via props

---
+++ src/data/home.ts
@@ -0,0 +1,8 @@
+const marketplaceMetrics = [
+  { label: "Collections", value: "12K+" },
+  { label: "Collectors", value: "46K+" },
+  { label: "NFTs Minted", value: "320K+" },
+  { label: "Volume Traded", value: "18K ETH" },
+];
+
+export { marketplaceMetrics };
