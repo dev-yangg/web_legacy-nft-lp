111ff2ed7752f2f149fb8a21318b563f0ef88ee3
yang <juliansalvador.dev@gmail.com>
2026-07-01 23:12:23 +0800

    feat(data): add twitter handle and verified props in creators prop

--- src/types.ts
+++ src/types.ts
@@ -7,11 +7,13 @@ interface CreatorsCard {
   image: ImageMetadata;
   alt: string;
   name: string;
+  xHandle: string;
   role: string;
   bio: string;
   collectionCount: number;
   floorPrice: number;
   currency: string;
+  isVerified?: boolean;
 }
 
 export type { Metric, CreatorsCard };
